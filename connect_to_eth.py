ecf633e9ef207dc5bdc3871803c23e9ff640b923
Cathy Li <fangyicathy@gmail.com>
2025-06-30 07:02:53 +0000

    update

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -3,6 +3,7 @@ from web3 import Web3
 from web3.middleware import ExtraDataToPOAMiddleware
 from web3.providers.rpc import HTTPProvider
 
+
 '''
 If you use one of the suggested infrastructure providers, the url will be of the form
 now_url  = f"https://eth.nownodes.io/{now_token}"
@@ -26,12 +27,15 @@ def connect_with_middleware(contract_json):
 
 	# TODO complete this method
 	# The first section will be the same as "connect_to_eth()" but with a BNB url
-	url_bnb = "https://bsc-testnet.publicnode.com"
+	url_bnb = "https://bsc-testnet.public.blastapi.io"
 	w3 = Web3(HTTPProvider(url_bnb))
+	w3.middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
 
 	# The second section requires you to inject middleware into your w3 object and
 	# create a contract object. Read more on the docs pages at https://web3py.readthedocs.io/en/stable/middleware.html
 	# and https://web3py.readthedocs.io/en/stable/web3.contract.html
+	
+	assert w3.is_connected(), f"Failed to connect to provider at {url_bnb}"
 	contract = w3.eth.contract(address=address, abi=abi)
 
 	return w3, contract
