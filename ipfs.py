f1b1ca8ef10f996a20397f57be47ebeafb59e97d
Cathy Li <fangyicathy@gmail.com>
2025-06-30 21:13:21 +0000

    updates

--- ipfs.py
+++ ipfs.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-# PINATA_JWT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI0MDFmZGY1Mi0wN2UwLTRkMTUtYmZjNi00NDMzMzY2NTZiNzMiLCJlbWFpbCI6ImZhbmd5aWxpQHNlYXMudXBlbm4uZWR1IiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInBpbl9wb2xpY3kiOnsicmVnaW9ucyI6W3siZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjEsImlkIjoiRlJBMSJ9LHsiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjEsImlkIjoiTllDMSJ9XSwidmVyc2lvbiI6MX0sIm1mYV9lbmFibGVkIjpmYWxzZSwic3RhdHVzIjoiQUNUSVZFIn0sImF1dGhlbnRpY2F0aW9uVHlwZSI6InNjb3BlZEtleSIsInNjb3BlZEtleUtleSI6IjIzNjg2MjM5NjEzMzg0NGRjMzJmIiwic2NvcGVkS2V5U2VjcmV0IjoiZTgxYmRmYzkzYjQ5YjNjZjY5M2QzOWNiNmQzNmIxNWRlMWIxNGRjZGZlZGU2N2FjYTAzNWJhYWQzMTNiM2I3OCIsImV4cCI6MTc4Mjg1MDA3OX0.Mc4BoHW8fLtFv9dTjFol7sYHnQ4U5kQqFCJHBpJlWDc"
+PINATA_JWT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI0MDFmZGY1Mi0wN2UwLTRkMTUtYmZjNi00NDMzMzY2NTZiNzMiLCJlbWFpbCI6ImZhbmd5aWxpQHNlYXMudXBlbm4uZWR1IiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInBpbl9wb2xpY3kiOnsicmVnaW9ucyI6W3siZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjEsImlkIjoiRlJBMSJ9LHsiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjEsImlkIjoiTllDMSJ9XSwidmVyc2lvbiI6MX0sIm1mYV9lbmFibGVkIjpmYWxzZSwic3RhdHVzIjoiQUNUSVZFIn0sImF1dGhlbnRpY2F0aW9uVHlwZSI6InNjb3BlZEtleSIsInNjb3BlZEtleUtleSI6IjIzNjg2MjM5NjEzMzg0NGRjMzJmIiwic2NvcGVkS2V5U2VjcmV0IjoiZTgxYmRmYzkzYjQ5YjNjZjY5M2QzOWNiNmQzNmIxNWRlMWIxNGRjZGZlZGU2N2FjYTAzNWJhYWQzMTNiM2I3OCIsImV4cCI6MTc4Mjg1MDA3OX0.Mc4BoHW8fLtFv9dTjFol7sYHnQ4U5kQqFCJHBpJlWDc"
 
 def pin_to_ipfs(data):
     assert isinstance(data,dict), f"Error pin_to_ipfs expects a dictionary"
@@ -9,10 +9,10 @@ def pin_to_ipfs(data):
     
     url = "https://api.pinata.cloud/pinning/pinJSONToIPFS"
 
-    # headers = {
-    #     "Authorization": f"Bearer {PINATA_JWT}",
-    #     "Content-Type": "application/json"
-    # }
+    headers = {
+        "Authorization": f"Bearer {PINATA_JWT}",
+        "Content-Type": "application/json"
+    }
 
     response = requests.post(url, headers=headers, json={"pinataContent": data})
     response.raise_for_status()
